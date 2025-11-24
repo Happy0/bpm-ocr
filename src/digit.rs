fff3b6c0d32a2a65e1da5eb55043edd34b7fd759
Gordon Martin <gordonhughmartin@gmail.com>
2025-11-24 22:19:55 +0000

    Continue the game of whackamole ;x

--- src/digit.rs
+++ src/digit.rs
@@ -25,8 +25,10 @@ pub fn parse_digit(image: &Mat, full_digit_location: Rect2i) -> Result<i32, Proc
     let total_filled_in_area = count_non_zero(&focused_digit)?;
     let total_area = full_digit_location.area();
 
+    println!("{}",  (total_filled_in_area as f32) / (total_area as f32));
+
     // If we're drawn a box around an area that's mostly filled in, then it's probably a 1
-    if (total_filled_in_area as f32) / (total_area as f32) > 0.80 {
+    if (total_filled_in_area as f32) / (total_area as f32) > 0.77 {
         return Ok(1);
     }
 
@@ -78,7 +80,7 @@ pub fn parse_digit(image: &Mat, full_digit_location: Rect2i) -> Result<i32, Proc
 
             let total_filled_in_area = count_non_zero(&focused_segment)?;
 
-            if (total_filled_in_area as f32 / rect.area() as f32) > 0.50 {
+            if (total_filled_in_area as f32 / rect.area() as f32) > 0.55 {
                 return Ok(1);
             } else {
                 return Ok(0);
@@ -98,9 +100,15 @@ pub fn parse_digit(image: &Mat, full_digit_location: Rect2i) -> Result<i32, Proc
     });
 
     match result {
-        Some(num) => Ok(num.1),
-        None => Err(ProcessingError::AppError(
+        Some(num) => {
+            println!("{}", num.1);
+
+            Ok(num.1)
+        },
+        None => {
+            println!("{:?}", lit_up);
+        Err(ProcessingError::AppError(
             crate::models::ReadingIdentificationError::CouldNotProcessSegments,
-        )),
+        ))},
     }
 }
